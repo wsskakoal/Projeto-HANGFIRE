fc9307db16e2b12d5632307ed9b4d7cfccee776e
wylliank <wyllian@kinyx.com.br>
2022-03-27 19:57:43 -0400

    Commit inicial

---
+++ Projeto HANGFIRE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_HANGFIRE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
